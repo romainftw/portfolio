788059ff25b64596ca841e2ed082fe162e2cf2ef
romain perrin <romain.nice@gmail.com>
2023-06-14 11:05:09 +0200

    js update

---
+++ js/script.js
@@ -0,0 +1,19 @@
+
+    fetch('public/navbar.html') // Utilisez le chemin relatif du fichier home.html à partir de l'emplacement de index.html
+      .then(response => response.text())
+      .then(data => {
+        const section1Container = document.getElementById('nav-container');
+        section1Container.innerHTML = data;
+      })
+      .catch(error => {
+        console.log('Erreur lors du chargement de home.html :', error);
+      });
+  fetch('public/home.html') // Utilisez le chemin relatif du fichier home.html à partir de l'emplacement de index.html
+    .then(response => response.text())
+    .then(data => {
+      const section1Container = document.getElementById('home-container');
+      section1Container.innerHTML = data;
+    })
+    .catch(error => {
+      console.log('Erreur lors du chargement de home.html :', error);
+    });
